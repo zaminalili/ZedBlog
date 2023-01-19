8a785381d5bf7aa138ccc37e4cd59a27709738cb
Zamin Alili <zaminalili29@gmail.com>
2023-01-19 10:40:28 +0400

    UnitOfWorks

---
+++ ZedBlog/ZedBlog.Data/UnitOfWorks/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using ZedBlog.Core.Entities;
+using ZedBlog.Data.Repositories;
+
+namespace ZedBlog.Data.UnitOfWorks
+{
+    public interface IUnitOfWork: IAsyncDisposable
+    {
+        Repository<T> GetRepository<T>() where T : class, IEntityBase, new();
+        Task<int> SaveAsync();
+        int Save();
+    }
+}
---
+++ ZedBlog/ZedBlog.Data/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZedBlog.Data.Context;
+using ZedBlog.Data.Repositories;
+
+namespace ZedBlog.Data.UnitOfWorks
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext dbContext;
+        public UnitOfWork(AppDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public ValueTask DisposeAsync()
+        {
+            return dbContext.DisposeAsync();
+        }
+
+        public int Save()
+        {
+            return dbContext.SaveChanges();
+        }
+
+        public Task<int> SaveAsync()
+        {
+            return dbContext.SaveChangesAsync();
+        }
+
+        Repository<T> IUnitOfWork.GetRepository<T>()
+        {
+            return new Repository<T>(dbContext);
+        }
+    }
+}
