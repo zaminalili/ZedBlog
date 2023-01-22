4692896f54566cdb4ed1d1a4d1175d53a3dee3f5
Zamin Alili <zaminalili29@gmail.com>
2023-01-22 16:28:46 +0400

    Admin Area

--- ZedBlog/ZedBlog.Web/Program.cs
+++ ZedBlog/ZedBlog.Web/Program.cs
@@ -38,8 +38,18 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+//app.MapControllerRoute(
+//    name: "default",
+//    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapAreaControllerRoute(
+        name: "Admin",
+        areaName: "Admin",
+        pattern: "Admin/{controller=Home}/{action=Index}/{id?}"
+        );
+    endpoints.MapDefaultControllerRoute();
+});
 
 app.Run();
